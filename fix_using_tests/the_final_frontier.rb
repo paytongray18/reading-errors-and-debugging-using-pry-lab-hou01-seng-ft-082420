df204b1f3c7fcec9e17edfa79b3a681ade0db3c7
Payton Gray <paytongray18@gmail.com>
2020-07-14 21:34:15 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def generate_star_date
+<<<<<<< HEAD
  puts (rand(100000) + 400000) / 10.0 
 end
 
@@ -9,6 +10,20 @@ def state_log(star_date)
 end
 
 def engage
+=======
+ puts (rand(100000) + 400000) / 10.0
+end
+
+
+def state_log(generate_star_date)
+ puts "Captain's Log, star date #{generate_star_date}."
+end
+
+def engage(state_log)
+  puts state_log(date)
+>>>>>>> 66d86a437ed448057f53d0f5b76103f373bc2d81
   date = generate_star_date
   puts state_log(date)
 end
+
+engage(state_log)
